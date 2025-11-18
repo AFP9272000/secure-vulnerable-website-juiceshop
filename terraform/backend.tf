ce855333486dd29f4354e82d2f8bd3fbf1d3d33f
Addison Fransesco Pirlo <addisonpirlo2@gmail.com>
2025-11-18 17:44:43 -0500

    Add files via upload

---
+++ terraform/backend.tf
@@ -0,0 +1,10 @@
+﻿# Terraform remote state backend for Juice Shop
+terraform {
+  backend "s3" {
+    bucket         = "afp9272000-juiceshop-tfstate-1763232924"
+    key            = "terraform.tfstate"
+    region         = "us-east-2"
+    encrypt        = true
+    dynamodb_table = "afp9272000-juiceshop-state-lock"
+  }
+}
